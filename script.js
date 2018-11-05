b91b09950b015123be48e953eb5db4aeb6e40c90
Martin Jordán <martin@jordan.cz>
2018-11-05 19:23:34 +0100

    refactor results

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const key = 'AIzaSyCRajoOl2QY5guIlKBggIOCTOhGUagSTPo';
 const cx = '008423077959082289852:1vtwjajtvtg';
 
 window.onload = function() {
-    var btn = document.getElementById('searchButton');
-    var input = document.getElementById('query');
+	var btn = document.getElementById('searchButton');
+	var input = document.getElementById('query');
 
 	input.addEventListener('keyup', function (event) {
 		event.preventDefault();
@@ -12,16 +12,16 @@ window.onload = function() {
 		}
 	});
 
-    btn.addEventListener('click', function () {
-    	clear();
-    	doRequest(generateSrc(), function (data) {appendWebResults(data); });
-    	doRequest(generateSrc(true), function(data) {appendImageResults(data); });
+	btn.addEventListener('click', function () {
+		clear();
+    	doRequest(generateSrc(), function (data) {appendResults(data); });
+    	doRequest(generateSrc(true), function(data) {appendResults(data, true); });
     });
 };
 
 function generateSrc(isImageSearch = false) {
 	var query = document.getElementById('query').value;
-    return 'https://www.googleapis.com/customsearch/v1?key=' + key + '&cx=' + cx + '&q=' + query + (isImageSearch ? '&searchType=image' : '');
+	return 'https://www.googleapis.com/customsearch/v1?key=' + key + '&cx=' + cx + '&q=' + query + (isImageSearch ? '&searchType=image' : '');
 };
 
 function doRequest(src, onDataLoaded) {
@@ -51,24 +51,25 @@ function clear() {
 	document.getElementById('images').innerHTML = "";
 };
 
-function appendWebResults(response) {
-	for (var i = 0; i < response.items.length; i++) {
-		var item = response.items[i];
-		var resultDiv = document.createElement('div');
-		resultDiv.className = 'webResult';
-		resultDiv.innerHTML += '<b>' + item.title + '</b>' + '<br>' + '<a href="' + item.formattedUrl + '" target="_blank" >' + item.formattedUrl + '</a>' + '<br>' + '<i>' + item.snippet + '</i>';
-		document.querySelector("div#webResults > div.rows").appendChild(resultDiv);
-	}
+function appendWebResult(item) {
+	var resultDiv = document.createElement('div');
+	resultDiv.className = 'webResult';
+	resultDiv.innerHTML += '<b>' + item.title + '</b>' + '<br>' + '<a href="' + item.formattedUrl + '" target="_blank" >' + item.formattedUrl + '</a>' + '<br>' + '<i>' + item.snippet + '</i>';
+	document.querySelector("div#webResults > div.rows").appendChild(resultDiv);
 };
 
-function appendImageResults(response) {
+function appendImageResult(item) {
+	var resultImage = document.createElement('img');
+	resultImage.src = item.link;
+	resultImage.href = item.link;
+	resultImage.className = 'imageResult';
+	document.querySelector("div#imageResults > div.images").appendChild(resultImage);
+};
+
+function appendResults(response, isImageSearch = false) {
 	for (var i = 0; i < response.items.length; i++) {
 		var item = response.items[i];
-		var resultImage = document.createElement('img');
-		resultImage.src = item.link;
-		resultImage.href = item.link;
-		resultImage.className = 'imageResult';
-		document.querySelector("div#imageResults > div.images").appendChild(resultImage);
+		(isImageSearch ? appendImageResult(item) : appendWebResult(item));
 	}
 };
 
